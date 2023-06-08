51550c94092383242218e8e3e54ea886349ad81e
Kirill Starodubsev <colereteps@gmail.com>
2023-06-08 14:28:05 +0400

    feat: Add some alias

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const dotenv = require('dotenv');
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
@@ -21,8 +22,20 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'style.css',
         }),
+        new webpack.DefinePlugin({
+            'process.env.SOCKET_SERVER_URL': JSON.stringify(process.env.SOCKET_SERVER_URL)
+        })
     ],
     resolve: {
+        alias: {
+            "@components": path.resolve(__dirname, 'src/components'),
+            "@pages": path.resolve(__dirname, 'src/pages'),
+            "@layout": path.resolve(__dirname, 'src/layout'),
+            "@utils": path.resolve(__dirname, 'src/utils'),
+            "@sockets": path.resolve(__dirname, 'src/sockets'),
+            "@interfaces": path.resolve(__dirname, 'src/interfaces'),
+            "@store": path.resolve(__dirname, 'src/store'),
+        },
         extensions: ['.tsx', '.ts', '.json', '.js'],
     },
     performance: {
