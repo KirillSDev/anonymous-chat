4b511abcb4191b3415e69ba48d28f81005aa2468
Kirill Starodubsev <colereteps@gmail.com>
2023-06-08 14:26:23 +0400

    feat: Add Layout

--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -1,6 +1,13 @@
 import { FC } from 'react';
 import { LayoutProps } from './Layout.props';
+import cn from 'classnames';
 import styles from './Layout.module.scss';
 export const Layout: FC<LayoutProps> = ({ children }): JSX.Element => {
-    return <div className='container-lg bg-white'></div>;
+    return (
+        <div className={cn(styles.container)}>
+            <div className='container-lg bg-light'>
+                <div className={cn('row', styles.row)}>{children}</div>
+            </div>
+        </div>
+    );
 };
